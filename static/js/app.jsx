4a3fef5779100f465a2aa8c5c8f97218a6f74504
Adriaan Mulder <adriaan.mulder@gmail.com>
2016-10-28 13:59:40 -0700

    add message components

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,4 +1,54 @@
+
+class Message extends React.Component {
+  render() {
+    return (
+      <li>{this.props.message}</li>
+    )
+  }
+}
+
+class MessageInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static propTypes = {
+    onTextChange: React.PropTypes.func.isRequired
+  }
+
+  textChange() {
+    console.log("hereerererere");
+  }
+
+  render() {
+    return (
+      <input type="text" onChange={this.textChange}/>
+    );
+  }
+}
+
+class Messenger extends React.Component {
+  onTextChange() {
+    console.log("text changed");
+  }
+  render() {
+    let messages = [
+      "hi there",
+      "hello",
+      "what's up",
+    ];
+    let messageList = messages.map((message, index) => {
+      return <Message message={message} key={index} />
+    });
+    return (
+      <div>
+       <ul> {messageList} </ul>
+        <MessageInput onTextChange={this.onTextChange} />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Messenger />,
   document.getElementById('app')
 );
