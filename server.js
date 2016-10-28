ed5ca20db0359687cb111be822613e7fe2c3dd92
Gavin Vickery <gavin@geekforbrains.com>
2016-10-28 13:42:17 -0700

    Updating index to server from statics.

--- server.js
+++ server.js
@@ -2,22 +2,12 @@ var express = require('express');
 var app = express();
 require('express-ws')(app);
 
-app.use(function (req, res, next) {
-  console.log('middleware');
-  req.testing = 'testing';
-  return next();
-});
-
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-  res.end();
-});
+app.use(express.static('static'));
 
 app.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
     console.log(msg);
   });
-  console.log('socket', req.testing);
 });
 
 app.listen(3000);
